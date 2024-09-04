ba30e42b93a228f30a44bf7e30db641f1c59ea9b
vladv <vladv@pointgrab.com>
2024-09-04 16:11:04 +0300

    Add script return

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # devops-r-d
 Learning DevOps practises
+
+1. Created repositiry devops-r-d
+2. Pushed the script return.py
---
+++ return.py
@@ -0,0 +1,7 @@
+def square_of_7():
+    print("I am before return")
+    return 7**2
+
+
+result = square_of_7()
+print(result)
